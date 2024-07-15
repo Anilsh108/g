3a74a0ee968a146a35698ac35789bda31f39fdeb
Anilsh108 <anilshkanaji@gmail.com>
2024-07-15 12:43:21 +0530

    first commit

---
+++ src/main/java/g/com/launch.java
@@ -0,0 +1,28 @@
+package g.com;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class launch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		   WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.facebook.com/");
+    
+        //Maximize the window
+        driver.manage().window().maximize();
+        // Verify the page title
+        String pageTitle = driver.getTitle();
+        System.out.println("Page Title: " + pageTitle);
+        // Close the browser window
+        driver.quit();
+
+
+	}
+
+}
